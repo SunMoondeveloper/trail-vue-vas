544d5b3176627a6ac408df8086cfc5320efb3b16
suresh <sunmoondeveloper@gmail.com>
2023-07-17 12:27:35 +0530

    Admin login page completed

--- vue3-ts-vite-starter-template/src/views/Home.vue
+++ vue3-ts-vite-starter-template/src/views/Home.vue
@@ -15,8 +15,8 @@
         class="d-none d-lg-flex align-items-center p-5"
       >
         <div class="w-100 d-lg-flex align-items-center justify-content-center px-5">
-          <b-img
-            fluid
+          <img
+            src="../assets/images/pages/login-v2-dark.svg"
             alt="Login V2"
           />
         </div>
